0140d67e82a67b3309847ec735a922010d40bf88
maverick0bg <maverick.bg@gmail.com>
2023-01-19 22:03:49 +0000

    cleanup unnecessary datasources

--- versions.tf
+++ versions.tf
@@ -42,14 +42,6 @@ provider "aws" {
   region = local.region
 }
 
-data "aws_eks_cluster" "eks_cluster" {
-  name = local.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks_kluster_auth" {
-  name = local.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
