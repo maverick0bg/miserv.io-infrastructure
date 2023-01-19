046fc0fd97851e9ca347c6a5d98c95d22486096c
maverick0bg <maverick.bg@gmail.com>
2023-01-19 17:08:12 +0200

    add oidc (#8)
    * add oidc
    
    * fix iam role reference

--- ElasticKS.tf
+++ ElasticKS.tf
@@ -36,3 +36,65 @@ module "eks" {
     }
   }
 }
+
+
+# Creates a kubernetes cluster role with necessary access to deploy
+resource "kubernetes_cluster_role" "github_oidc_cluster_role" {
+    metadata {
+        name = "github-oidc-cluster-role"
+    }
+
+    rule {
+        api_groups  = ["*"]
+        resources   = ["deployments","pods","services"]
+        verbs       = ["get", "list", "watch", "create", "update", "patch", "delete"]
+    }
+}
+
+# Creates a cluster role binding between the above kubernetes cluster role and the user
+resource "kubernetes_cluster_role_binding" "github_oidc_cluster_role_binding" {
+  metadata {
+    name = "github-oidc-cluster-role-binding"
+  }
+
+  subject {
+    kind = "User"
+    name =  "github-oidc-auth-user"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.github_oidc_cluster_role.metadata[0].name
+  }
+}
+
+resource "kubernetes_config_map" "aws-auth" {
+  data = {
+    "mapRoles" = yamlencode([
+      {
+        "groups": ["system:bootstrappers", "system:nodes"],
+        "rolearn": aws_iam_role.github_oidc_auth_role.arn
+        "username": "system:node:{{EC2PrivateDNSName}}"
+      },
+      {
+        "rolearn": aws_iam_role.github_oidc_auth_role.arn
+        "username": "github-oidc-auth-user"
+        
+      }
+    ])
+
+    "mapAccounts" = yamlencode([])
+    "mapUsers" = yamlencode([])
+  }
+
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/managed-by" = "Terraform"
+      "terraform.io/module"          = "terraform-aws-modules.eks.aws"
+    }
+  }
+}
