bac14fc8c1f964558b3209431602079211d7c9ef
maverick0bg <maverick.bg@gmail.com>
2023-01-20 15:13:24 +0000

    remove crashing resources

--- ElasticKS.tf
+++ ElasticKS.tf
@@ -15,50 +15,50 @@ module "eks" {
   }
 }
 
-# Creates a kubernetes cluster role with necessary access to deploy
-resource "kubernetes_cluster_role" "github_oidc_cluster_role" {
-  metadata {
-    name = "github-oidc-cluster-role"
+# # Creates a kubernetes cluster role with necessary access to deploy
+# resource "kubernetes_cluster_role" "github_oidc_cluster_role" {
+#   metadata {
+#     name = "github-oidc-cluster-role"
 
-  }
+#   }
 
-  rule {
-    api_groups = ["*"]
-    resources  = ["*"]
-    verbs      = ["*"]
-  }
-}
+#   rule {
+#     api_groups = ["*"]
+#     resources  = ["*"]
+#     verbs      = ["*"]
+#   }
+# }
 
-# Creates a cluster role binding between the above kubernetes cluster role and the user
-resource "kubernetes_cluster_role_binding" "github_oidc_cluster_role_binding" {
-  metadata {
-    name = "github-oidc-cluster-role-binding"
-  }
+# # Creates a cluster role binding between the above kubernetes cluster role and the user
+# resource "kubernetes_cluster_role_binding" "github_oidc_cluster_role_binding" {
+#   metadata {
+#     name = "github-oidc-cluster-role-binding"
+#   }
 
-  subject {
-    kind      = "User"
-    name      = "github-oidc-auth-user"
-    api_group = "rbac.authorization.k8s.io"
-  }
+#   subject {
+#     kind      = "User"
+#     name      = "github-oidc-auth-user"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
 
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "kube-system"
-  }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "default"
+#     namespace = "kube-system"
+#   }
 
-  subject {
-    kind      = "Group"
-    name      = "system:masters"
-    api_group = "rbac.authorization.k8s.io"
-  }
+#   subject {
+#     kind      = "Group"
+#     name      = "system:masters"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.github_oidc_cluster_role.metadata[0].name
-  }
-}
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.github_oidc_cluster_role.metadata[0].name
+#   }
+# }
 
 resource "kubernetes_config_map" "aws-auth" {
   data = {
