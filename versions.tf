071a1ade7809fedda10ac0531e37ac80a52be0ff
maverick0bg <maverick.bg@gmail.com>
2023-01-20 15:10:17 +0000

    try with native token

--- versions.tf
+++ versions.tf
@@ -53,12 +53,12 @@ data "aws_eks_cluster_auth" "default" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
-    command     = "aws"
-  }
-
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+  #   command     = "aws"
+  # }
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 data "aws_availability_zones" "available" {}
