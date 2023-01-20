b60456a4890119549e7d64b03c91271f0aaa7d51
maverick0bg <maverick.bg@gmail.com>
2023-01-20 14:58:48 +0000

    switch to datasources for the eks cluster when kube login

--- versions.tf
+++ versions.tf
@@ -42,9 +42,17 @@ provider "aws" {
   region = local.region
 }
 
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.default.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
