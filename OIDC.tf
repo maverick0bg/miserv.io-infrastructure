71a3c770b67394182da10c310ff2d6b4bb7cebb3
maverick0bg <maverick.bg@gmail.com>
2023-01-21 21:12:10 +0000

    fix snake case

--- OIDC.tf
+++ OIDC.tf
@@ -83,34 +83,34 @@ resource "aws_iam_role" "amazon_role" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.amazon_role.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_eks_vpc_resource_controller" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.amazon_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_eks_worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.amazon_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.amazon_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_ec2_container_registry_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.amazon_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_role_AmazonEKSFargatePodExecutionRolePolicy" {
+resource "aws_iam_role_policy_attachment" "amazon_role_amazon_eks_fargate_pod_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
   role       = aws_iam_role.amazon_role.name
 }
@@ -139,7 +139,7 @@ resource "aws_iam_role" "ebs_csi_driver_role" {
   POLICY1
 }
 
-resource "aws_iam_role_policy_attachment" "atachment_AmazonEKS_EBS_CSI_DriverRole" {
+resource "aws_iam_role_policy_attachment" "atachment_amazon_eks_ebs_csi_driver_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi_driver_role.name
 }
