663e0a39bdfd1bd0522af1576ddb3722ae2ecb76
maverick0bg <maverick.bg@gmail.com>
2023-01-21 19:12:08 +0000

    cleanup and tune

--- tobs.tf
+++ tobs.tf
@@ -3,10 +3,9 @@ resource "helm_release" "tobs" {
   name       = "tobs"
   repository = "https://charts.timescale.com/"
   chart      = "timescale/tobs"
-  namespace = "tobs"
+  version    = "20.7.0"
+  namespace  = "tobs"
   values = [
     file("${path.module}/tobs_values.yaml")
   ]
-
-
 }
