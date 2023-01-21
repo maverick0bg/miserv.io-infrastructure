7dd7076764679b23b49db7e57d512888980164d8
maverick0bg <maverick.bg@gmail.com>
2023-01-21 08:27:22 +0000

    authenticate with exec plugin

--- versions.tf
+++ versions.tf
@@ -35,7 +35,7 @@ terraform {
     }
 
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = "~>2.8.0"
     }
   }
@@ -57,6 +57,12 @@ resource "random_string" "suffix" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
   }
 }
